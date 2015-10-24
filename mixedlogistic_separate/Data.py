d53734a92215d947e67beb8bd9fe7c37d63047b9
Pan Chao <panchao25@gmail.com>
2015-10-24 19:38:02 -0400

    Added __str__ method for Data class.

--- mixedlogistic_separate/Data.py
+++ mixedlogistic_separate/Data.py
@@ -5,3 +5,23 @@ class Data(object):
         self.y = y
         self.m = m
         self.n = len(y)
+
+    def _formatArray(self, x):
+        if self.n < 5:
+            return '\n'.join('{}:\t{}'.format(*example) for example in enumerate(x))
+        else:
+            s1 = '\n'.join('{}:\t{}'.format(*example) for example in enumerate(x[:3]))
+            s2 = '\n'.join('{}:\t{}'.format(i+self.n-3, val) for (i, val) in enumerate(x[self.n-3:]))
+            return '\n'.join([s1, '...', s2])
+
+    def __str__(self):
+        line1 = "Hidden layer covariates:"
+        line2 = self._formatArray(self.Xm) if self.Xm is not None else 'None'
+
+        line3 = "Observed layer covariates:"
+        line4 = self._formatArray(self.Xr) if self.Xr is not None else 'None'
+
+        line5 = "Response with {} categories:".format(self.m+1)
+        line6 = self._formatArray(self.y)
+
+        return '\n'.join([line1, line2, line3, line4, line5, line6])
